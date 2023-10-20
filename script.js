f052b5eab8e6d877226140c1f512d8296c1933f0
Ashish Kumar Ram <74091038+Ashu007A@users.noreply.github.com>
2023-10-20 12:05:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 //your code here
 
+document.addEventListener("DOMContentLoaded", function() {
+  // Array of band names
+  let bandNames = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+  // Function to remove articles and return the name without articles
+  function removeArticles(name) {
+    return name.replace(/^(a |an |the )/i, '').trim();
+  }
+
+  // Custom sort function to sort the band names
+  bandNames.sort(function(a, b) {
+    const nameA = removeArticles(a);
+    const nameB = removeArticles(b);
+    return nameA.localeCompare(nameB);
+  });
+
+  // Get the ul element
+  const bandList = document.getElementById('band');
+
+  // Populate the ul element with sorted band names using li elements
+  bandNames.forEach(function(bandName) {
+    const li = document.createElement('li');
+    li.textContent = bandName;
+    bandList.appendChild(li);
+  });
+});
